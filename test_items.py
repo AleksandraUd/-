8df4436539f601daac4942b345c9d304a8484ed9
AleksandraUd <aleksandra.udalova@convergent-usa.com>
2019-12-19 09:54:42 +0300

    Изменила название тестового метода внутри файла test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser, request):
+def test_guest_should_see_add_to basket_button(browser):
 	browser.get(link)
 	button = browser.find_elements_by_css_selector(".btn-add-to-basket")
 	assert len(button)>0, 'There is no button'
